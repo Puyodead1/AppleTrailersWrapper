d767d1d36af898838abb935b2117ec217844ccc6
Puyodead1 <23562356riley@gmail.com>
2020-01-04 12:21:23 -0500

    update

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AppleTrailersWrapper",
-    version="1.0.2",
+    version="1.0.3",
     author="Puyodead1",
     author_email="puyodead@protonmail.com",
     description="Simple package that wraps Apple Trailers",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Puyodead1/AppleTrailersWrapper",
-    packages=setuptools.find_packages("."),
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Development Status :: 4 - Beta"
     ],
-    python_requires='>=3.6',
+    python_requires='>=3',
 )
