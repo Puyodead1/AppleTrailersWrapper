e397c1b28ec6feb723d0309b91bdb38891bece03
Puyodead1 <23562356riley@gmail.com>
2020-01-04 19:19:52 -0500

    Update docs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AppleTrailersWrapper",
-    version="1.0.3",
+    version="1.0.4",
     author="Puyodead1",
     author_email="puyodead@protonmail.com",
     description="Simple package that wraps Apple Trailers",
